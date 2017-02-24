4242035c1eca58c32d10b50282740d8ecc634263
wanghui <wh_linux@126.com>
2017-02-24 15:42:03 +0800

    Update fabfile deploy rule

--- fabfile.py
+++ fabfile.py
@@ -1,60 +1,34 @@
-from fabric.api import *
-import fabric.contrib.project as project
-import os
+from fabric.api import sudo, cd, settings, env, task
 
-# Local path configuration (can be absolute or relative to fabfile)
-env.deploy_path = 'output'
-DEPLOY_PATH = env.deploy_path
+env.use_ssh_config = True
 
-# Remote server configuration
-production = 'root@localhost:22'
-dest_path = '/var/www'
-
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = 'my_rackspace_username'
-env.cloudfiles_api_key = 'my_rackspace_api_key'
-env.cloudfiles_container = 'my_cloudfiles_container'
+repo_uri = "https://github.com/coldnight/coldnight.github.com"
+dest_path = "/srv/coldnight.github.com.new"
 
 
-def clean():
-    if os.path.isdir(DEPLOY_PATH):
-        local('rm -rf {deploy_path}'.format(**env))
-        local('mkdir {deploy_path}'.format(**env))
+@task
+def deploy():
+    """Deploy to serve"""
 
-def build():
-    local('pelican -s pelicanconf.py')
+    with settings(warn_only=True):
+        if sudo("test -d %s" % dest_path).failed:
+            sudo("git clone  %s %s" % (repo_uri, dest_path))
 
-def rebuild():
-    clean()
-    build()
+        if sudo("virtualenv --version").failed:
+            sudo("pip install -U virtualenv")
 
-def regenerate():
-    local('pelican -r -s pelicanconf.py')
+    with cd(dest_path):
+        sudo("git fetch origin")
+        sudo("git checkout master && git merge origin/master")
+        sudo("git submodule init && git submodule update --remote --merge")
+        with settings(warn_only=True):
+            if sudo("test -d .venv").failed:
+                sudo("virtualenv .venv")
 
-def serve():
-    local('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
+        commands = [
+            "source .venv/bin/activate",
+            "pip install -U pelican",
+            "make html"
+        ]
 
-def reserve():
-    build()
-    serve()
-
-def preview():
-    local('pelican -s publishconf.py')
-
-def cf_upload():
-    rebuild()
-    local('cd {deploy_path} && '
-          'swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-          '-U {cloudfiles_username} '
-          '-K {cloudfiles_api_key} '
-          'upload -c {cloudfiles_container} .'.format(**env))
-
-@hosts(production)
-def publish():
-    local('pelican -s publishconf.py')
-    project.rsync_project(
-        remote_dir=dest_path,
-        exclude=".DS_Store",
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True
-    )
+        sudo(" && ".join(commands))
